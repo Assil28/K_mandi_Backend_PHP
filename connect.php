1192a577491cd2ca3fb2984e4952020ff6554ba7
Assil Dkhil <assil.dkhil28@gmail.com>
2024-06-23 20:43:11 +0100

    how to connect to mangoDB

--- connect.php
+++ connect.php
@@ -21,3 +21,43 @@ try {
 } catch (PDOException $e) {
    echo $e->getMessage();
 }
+
+
+
+// connecter a mongoDB 
+
+// installer ceci a travers le terminal : 
+// sudo pecl install mongodb
+// composer require mongodb/mongodb
+
+//puis
+
+// <?php
+// require 'vendor/autoload.php'; // Assurez-vous d'avoir installé le client MongoDB via Composer
+
+// $countrowinpage = 9;
+// try {
+//     // Connexion à MongoDB
+//     $client = new MongoDB\Client("mongodb://localhost:27017");
+//     $mongodb = $client->ecommerce; // Nom de la base de données MongoDB
+//     $collection = $mongodb->products; // Nom de la collection MongoDB
+
+//     // Définir les en-têtes CORS
+//     header("Access-Control-Allow-Origin: *");
+//     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Origin");
+//     header("Access-Control-Allow-Methods: POST, OPTIONS , GET");
+    
+//     include "functions.php";
+
+//     // Pour l'authentification si nécessaire
+//     // if (!isset($notAuth)) {
+//     //    checkAuthenticate();
+//     // }
+
+//     // Exemple de récupération de documents depuis MongoDB
+//     $documents = $collection->find()->toArray();
+//     echo json_encode($documents);
+
+// } catch (MongoDB\Exception\Exception $e) {
+//     echo "Erreur MongoDB : " . $e->getMessage();
+// }
