00d30636c5e9b304d7b9fa8d651625157ea96630
Assil Dkhil <assil.dkhil28@gmail.com>
2024-08-01 21:03:46 +0100

    Shearch for product with English or Arabic name

--- items/search.php
+++ items/search.php
@@ -4,6 +4,9 @@ include "../connect.php" ;
 
 $search = filterRequest("search") ; 
 
+
+// find item by English name or Arabic name
+
 getAllData("items1view" , "items_name LIKE '%$search%' OR items_name_ar  LIKE '%$search%'  ") ; 
 
 ?>
