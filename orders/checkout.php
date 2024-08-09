11103360774d379d000bd234373b71fe97d9a33f
Assil Dkhil <assil.dkhil28@gmail.com>
2024-08-09 10:19:19 +0100

    orders checkout

--- orders/checkout.php
+++ orders/checkout.php
@@ -24,8 +24,6 @@ $totalprice = $ordersprice  + $pricedelivery;
 $now = date("Y-m-d H:i:s");
 
 $checkcoupon = getData("coupon", "coupon_id = '$couponid' AND coupon_expiredate > '$now' AND coupon_count > 0  ", null,  false);
-
-
 if ($checkcoupon  > 0) {
     $totalprice =  $totalprice - $ordersprice * $coupondiscount / 100;
     $stmt = $con->prepare("UPDATE `coupon` SET  `coupon_count`= `coupon_count` - 1  WHERE coupon_id = '$couponid' ");
@@ -46,15 +44,18 @@ $data = array(
     "orders_paymentmethod"  =>  $paymentmethod
 );
 
+//false bech le yaa3mel print f retour 
 $count = insertData("orders", $data, false);
 
+
+// ken sar e checkout ( 3adina lordre )
 if ($count > 0) {
 
     $stmt = $con->prepare("SELECT MAX(orders_id) from orders ");
     $stmt->execute();
     $maxid = $stmt->fetchColumn();
 
-    $data = array("cart_orders" => $maxid);
+    $data = array("cart_orders" => $);
 
     updateData("cart", $data, "cart_usersid = $usersid  AND cart_orders = 0 ");
 }
