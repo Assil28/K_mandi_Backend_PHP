0e9efb0f37c6fbcab23e757643fba3be91c2488c
Assil Dkhil <assil.dkhil28@gmail.com>
2024-08-13 15:21:45 +0100

    admin approuve user's order

--- admin/orders/approve.php
+++ admin/orders/approve.php
@@ -2,10 +2,14 @@
 
 include "../../connect.php";
 
+
+// accepter lordre du client
+
 $orderid = filterRequest("ordersid");
 
 $userid = filterRequest("usersid");
 
+// client k y3adi commande  l order_status tkoun 0 , wa9et l admin accpte l order yarja3 1
 $data = array(
     "orders_status" => 1
 );
